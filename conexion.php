2db8a41aa8f2b7c14b6d45ba90fd7d5392202fba
bomj321 <mauricio1314@hotmail.com>
2018-12-11 01:27:28 -0400

    Ultimos detalles

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
-$connection = mysqli_connect('localhost', 'root', '', 'sistema_archivos');
-  
+$connection = mysqli_connect('localhost', 'root', '', 'sistema_archivo');
+
 if (!$connection) {
     die("Database Connection Failed" . mysqli_error($connection));
 }
